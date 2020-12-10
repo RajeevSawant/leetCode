c77415c4b6d1fcaa64a174f69fbfe93eb25cbfa0
Rajeev Sawant <rajeevsawant@Rajeevs-MacBook-Pro.local>
2020-12-10 15:14:35 -0800

    (Day-13): Revision-Day13-Active-Recall SD:10-Dec ED:10-Dec

--- Day40_10292020-BM/Maximum-xor-of-two-num-in-array.cpp
+++ Day40_10292020-BM/Maximum-xor-of-two-num-in-array.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+
+Based on this solution: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/91049/Java-O(n)-solution-using-bit-manipulation-and-HashMap
+
+I think this question looks like the most difficult question until you get to the right track. The idea is you try to find an answer based on possible candidates. I will try to briefly explain it using 8 bits instead of 32 bits.
+
+Assume that we have 8 bit ints. Our max result could be 11111111. To achieve this there must be two numbers when XORed their MSB should give 1.
+
+So our first mask is 1000000. (1 shifted all the way to the left). Since we are only checking the first digit we AND the nums with this mask and add to a hash table. Then for each number in our input we test if there's a corresponding number that would give this. If there's then our next candidate is 11000000, if not then our next candidate is 0100000.
+
+The trick is that while we are using the hash table we search for " candidate ^ *it " if this exists then it means there is a " nums[x] ^ *it " that gives the candidate based on the XOR property A = B ^ C => A ^ C = B
+
+So we iterate through all the possible candidates. The outer loop can start from 30 or 31.
+
+*/
+
+
+int findMaximumXOR(vector<int>& nums) {
+
+        int mask = 0;
+        int result = 0;
+        for (int i=30; i>=0; i--)
+        {
+            mask =  mask | (1 << i);
+            unordered_set<int> myset;
+            for (int i=0; i<nums.size(); i++)
+            {
+                myset.insert(nums[i] & mask);
+            }
+
+            int candidate = result | (1<<i);
+            for (auto it=myset.begin(); it!=myset.end(); it++)
+            {
+                if (myset.count(candidate ^ *it))
+                {
+                    result = candidate;
+                    break;
+                }
+            }
+        }
+
+        return result;
+
+}
+
+
+
+
+
+/*
 class TrieNode
 {
 	int val;
@@ -71,3 +122,6 @@ int findMaximumXOR(vector<int>& nums)
 	
 	return res;
 }
+
+*/
+
--- Day48_11072020-Ary/Next-Permutation.cpp
+++ Day48_11072020-Ary/Next-Permutation.cpp
@@ -12,7 +12,7 @@ Swap nums[k] and nums[l].
 Reverse the sub-array nums[k + 1:].
  *
  *
- */
+ *
 
 
 
@@ -44,3 +44,81 @@ void nextPermutation(vector<int>& nums)
 
 	}
 }
+
+
+
+*/
+
+
+
+
+/*
+I don't think any one can understand this solution without seeing an example, here is an example:
+2,3,6,5,4,1
+
+Solution:
+Step1, from right to left, find the first number which not increase in a ascending order. In this case which is 3.
+Step2, here we can have two situations:
+
+We cannot find the number, all the numbers increasing in a ascending order. 
+This means this permutation is the last permutation, we need to rotate back to the first permutation. 
+So we reverse the whole array, for example, 6,5,4,3,2,1 we turn it to 1,2,3,4,5,6.
+
+We can find the number, then the next step, we will start from right most to leftward, try to find the first number which is larger than 3, 
+in this case it is 4.
+Then we swap 3 and 4, the list turn to 2,4,6,5,3,1.
+Last, we reverse numbers on the right of 4, we finally get 2,4,1,3,5,6.
+
+Time complexity is: O(3*n)=O(n).
+
+At the end, I don't know how to come up this solution. Here is just to understand the solution with example. Hope this helps.
+
+ 
+ *
+ *
+ */
+
+
+
+
+
+
+ void nextPermutation(int[] nums) {
+      int n = nums.length - 1, p = -1, pv = 0;
+
+      for(int i = n - 1; i >= 0; i--){
+        if(nums[i] < nums[i + 1]) {
+            p = i;
+            pv = nums[i];
+            break;
+        }
+      }
+
+      if(p == -1) {
+        reverse(nums, 0, n);
+        return;
+      }
+
+      for(int i = n; i >= 0; i--){
+        if(nums[i] > pv){
+          swap(nums, p, i);
+          break;
+        }
+      }
+
+      reverse(nums, p + 1, n);
+ }
+
+    void reverse(int[] nums, int s, int e){
+      while(s < e){
+        swap(nums, s, e);
+        s++;
+        e--;
+      }
+    }
+
+    void swap(int[] nums, int s, int e){
+        int t = nums[s];
+        nums[s] = nums[e];
+        nums[e] = t;
+    }

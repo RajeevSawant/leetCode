98c59fcd8d44d11b39b32e136dde6c5de9b4d329
Rajeev Sawant <rajeevsawant@Rajeevs-MacBook-Pro.local>
2021-01-04 07:21:20 -0800

    (Day-4): Revision: Day58 + Day62 = 4prb  SD:4-Jan ED:4-Jan

--- Day58_11212020-HT/Intersection-of-Two-Arrays-II.cpp
+++ Day58_11212020-HT/Intersection-of-Two-Arrays-II.cpp
@@ -27,3 +27,42 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
 
 	return out;
 }
+
+
+
+/*
+ 
+  C program:
+
+ #define Hashlength  1000
+
+
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+
+    int* ret;
+    int hash[Hashlength] = {0};
+
+
+    for (int i = 0; i < nums1Size; i++)
+    {
+        hash[nums1[i]]++;
+    }
+
+    ret = calloc(fmax(nums1Size, nums2Size), sizeof(int));
+    *returnSize = 0;
+
+
+    for (int j = 0; j < nums2Size; j++)
+    {
+        if (hash[nums2[j]] > 0)
+        {
+            ret[(*returnSize)++] = nums2[j];
+            hash[nums2[j]]--;
+        }
+    }
+
+
+    return ret;
+
+
+ */
--- Day58_11212020-HT/Intersection-of-two-Arrays.cpp
+++ Day58_11212020-HT/Intersection-of-two-Arrays.cpp
@@ -22,3 +22,38 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
 
 	return res;
 }
+
+
+/*
+
+C program:
+
+#define Hashlength 1000
+
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+
+    int* ret;
+    int hash[Hashlength] = {0};
+    
+    for (int i = 0; i < nums1Size; i++)
+    {
+        hash[nums1[i]] = 1;
+    }
+    
+    ret = calloc(fmax(nums1Size, nums2Size),  sizeof(int));
+    *returnSize = 0;
+    
+    for (int j = 0; j < nums2Size; j++)
+    {
+        if (hash[nums2[j]] == 1)
+        {
+            ret[(*returnSize)++] = nums2[j];
+            hash[nums2[j]] = 0;
+        }
+    }
+    
+    return ret;
+}
+ 
+ 
+ */
